55fc6cb828d54251ef12d1b8140a51ea1476a1b8
“mayluv17” <mayowaibrahim009@gmail.com>
2023-03-08 18:38:19 +0100

    Server side Generation

--- next-prerendering/pages/news/[category].js
+++ next-prerendering/pages/news/[category].js
@@ -1,5 +1,18 @@
-function ArticleListByCategory() {
-  return <div>Category</div>;
+function ArticleListByCategory({ category, articles }) {
+  return (
+    <>
+      <h1>Swowing news for {category} category</h1>
+      {articles.map((article) => {
+        return (
+          <div key={article.id}>
+            {article.id} {article.category} {article.category}
+            <p>{article.description}</p>
+            <hr />
+          </div>
+        );
+      })}
+    </>
+  );
 }
 
 export default ArticleListByCategory;
@@ -7,7 +20,7 @@ export default ArticleListByCategory;
 export async function getServerSideProps(context) {
   const { params } = context;
   const { category } = params;
-  const res = await fetch(`http://localhost:4000/category=${category}`);
+  const res = await fetch(`http://localhost:4000/news?category=${category}`);
   const data = await res.json();
 
   return {
