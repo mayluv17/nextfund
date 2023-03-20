be25ab2b14a4f5450bc7ffbe4f30fad497599bc9
“mayluv17” <mayowaibrahim009@gmail.com>
2023-03-20 20:25:39 +0100

    global css module

---
+++ styling/pages/about.js
@@ -0,0 +1,11 @@
+function About() {
+  return (
+    <div>
+      <h2>About us Page </h2>
+      <button className="btn btn-primary">some new button</button>
+    </div>
+  );
+}
+
+export default About;
+``;
