8056a1e5b354d6f76eb2eddef8c22cccc16a6aab
“mayluv17” <mayowaibrahim009@gmail.com>
2023-03-26 11:48:30 +0100

    scss

--- styling/pages/about.js
+++ styling/pages/about.js
@@ -1,8 +1,10 @@
+import styles from "../styles/About.module.scss";
 function About() {
   return (
     <div>
       <h2>About us Page </h2>
       <button className="btn btn-primary">some new button</button>
+      <h2 className={styles.highlightscss}>with higlighted text below</h2>
     </div>
   );
 }
